654e652518e40b26db7b05425f20f5be659c82b6
calvin56 <946405499@qq.com>
2020-11-26 07:51:05 +0800

    1
    Signed-off-by: calvin56 <946405499@qq.com>

--- src/main/java/com/example/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/example/rabbitmq/RabbitmqApplication.java
@@ -2,6 +2,9 @@ package com.example.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
 
 @SpringBootApplication
 public class RabbitmqApplication {
@@ -9,6 +12,12 @@ public class RabbitmqApplication {
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqApplication.class, args);
         System.out.println("\n------------------------- 项目启动成功 -----------------------\n");
+        try {
+            String simheiPath = ResourceUtils.getFile("classpath:simhei.ttf").getPath();
+            System.out.println(simheiPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
