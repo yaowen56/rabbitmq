758d216b34d6e01a258211626477cd10f9b74d97
calvin56 <946405499@qq.com>
2020-11-26 14:47:34 +0800

    w
    Signed-off-by: calvin56 <946405499@qq.com>

--- src/main/java/com/example/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/example/rabbitmq/RabbitmqApplication.java
@@ -3,8 +3,11 @@ package com.example.rabbitmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.ResourceUtils;
+import org.xhtmlrenderer.pdf.ITextFontResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 @SpringBootApplication
 public class RabbitmqApplication {
@@ -13,9 +16,12 @@ public class RabbitmqApplication {
         SpringApplication.run(RabbitmqApplication.class, args);
         System.out.println("\n------------------------- 项目启动成功 -----------------------\n");
         try {
-            String simheiPath = ResourceUtils.getFile("classpath:simhei.ttf").getPath();
-            System.out.println(simheiPath);
-        } catch (FileNotFoundException e) {
+            // String simheiPath = ResourceUtils.getFile("classpath:simhei.ttf").getPath();
+
+            ITextRenderer render = new ITextRenderer();
+            ITextFontResolver fontResolver = render.getFontResolver();
+            fontResolver.addFont("simhei.ttf", "Identity-H", false);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
